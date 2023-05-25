7fc7a4c205bd22de917d371ce3acbc4dfb4f1c80
taufikhm58 <taufikhidayatulmunir@gmail.com>
2023-05-25 23:02:36 +0700

    Feature Post Selesai

---
+++ QE10-GroupProject-TEAM4/src/test/java/starter/Gorest/post/PostsStepDef/DeleteUserPostStepDef.java
@@ -0,0 +1,34 @@
+package starter.Gorest.post.PostsStepDef;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Steps;
+import starter.Gorest.post.DeleteApi;
+
+public class DeleteUserPostStepDef {
+
+    @Steps
+    DeleteApi deleteApi;
+
+    @Given("Delete single user with valid and existing parameter {int}")
+    public void deleteSingleUserWithValidAndExistingParameter(int id) {
+        deleteApi.deleteValidUser(id);
+    }
+
+    @When("Send delete single user post")
+    public void sendDeleteSingleUserPost() {
+        SerenityRest.when().delete(DeleteApi.DELETE_USER_POST);
+    }
+
+    @Then("Status code should be {int} No Content")
+    public void statusCodeShouldBeNoContent(int noContent) {
+        SerenityRest.then().statusCode(noContent);
+    }
+
+    @Given("Delete single user with invalid {string}")
+    public void deleteSingleUserWithInvalid(String id) {
+        deleteApi.deleteInvalidUser(id);
+    }
+}
