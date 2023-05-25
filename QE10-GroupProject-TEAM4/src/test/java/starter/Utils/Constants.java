a422ec96f991504088efa4a8117b4155b4153498
Khairun Najmi Mukaffa <knmukafagmail.comgit config --global user.email knmukafagmail.com>
2023-05-25 18:16:23 +0700

    adding constant

--- QE10-GroupProject-TEAM4/src/test/java/starter/Utils/Constants.java
+++ QE10-GroupProject-TEAM4/src/test/java/starter/Utils/Constants.java
@@ -1,5 +1,10 @@
 package starter.Utils;
 
 public class Constants {
+    public static String BASE_URL = "https://gorest.co.in";
+    public static final String DIR = System.getProperty("user.dir");
 
+    public static String REQ_BODY_DIR = DIR+"/src/test/resources/JSON/ReqBody/";
+
+    public static String JSON_SCHEMA_DIR = DIR+"/src/test/resources/JSON/JSONSchema/";
 }
